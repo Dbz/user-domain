7f0c56e2f28f28cd7aea062f02af953ca91e4af0
Daniel <BurtDaniel@Gmail.com>
2015-01-24 14:11:18 -0800

    bug fix; removed dead code

--- main.js
+++ main.js
@@ -12,16 +12,14 @@ function refresh(f) {
 
 var main = function(){
   gmail = new Gmail();
-  console.log('Hello,', gmail.get.user_email())
+  console.log("user-domain has loaded");
   gmail.observe.on("open_email", function(id, url, body, xhr) {
-    // console.log("id:", id, "url:", url, 'body', body, 'xhr', xhr);
-    // console.log(gmail.get.email_data(id).people_involved);
     
     var emails = [];
     gmail.get.email_data(id).people_involved.forEach(function(arr) {
       if(arr[1].substring(arr[1].length - 9) != "gmail.com") {
         emails.push(arr[1]);
-        console.log(arr[1]);
+        // console.log(arr[1]);
       }
     });
     
@@ -31,7 +29,7 @@ var main = function(){
         var url = address.substring(address.indexOf("@") + 1);
         var $a = $('<a href="http://' + url + '"> ' + url + '</a>');
         $(this).parent().append($a);
-        var $i = $('<img class="fav" src="http://www.google.com/s2/favicons?domain=www.' + url + '">');
+        var $i = $('<img class="fav" src="http://www.google.com/s2/favicons?domain=' + url + '">');
         $(this).parent().prepend($i);
       });
     });
